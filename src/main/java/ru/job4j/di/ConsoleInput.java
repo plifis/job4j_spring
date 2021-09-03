8b22df0e1d37686d23aa5019080c4ada7d7afc10
plifis <nikich85@inbox.ru>
2021-09-03 12:59:51 +0300

    2. Сканирование проекта [#1070]

--- src/main/java/ru/job4j/di/ConsoleInput.java
+++ src/main/java/ru/job4j/di/ConsoleInput.java
@@ -1,8 +1,11 @@
 package ru.job4j.di;
 
 
+import org.springframework.stereotype.Component;
+
 import java.util.Scanner;
 
+@Component
 public class ConsoleInput {
     private Scanner scanner = new Scanner(System.in);
 
